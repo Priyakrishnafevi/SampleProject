7f9d0b20796e4219e925c5ded2171b51b45c70f4
Priya <131609246+Priyakrishnafevi@users.noreply.github.com>
2024-01-12 14:55:28 +0000

    Debug

---
+++ Sample_MavenProject/src/main/java/mavenpackage/LoopDebugging.java
@@ -0,0 +1,15 @@
+package mavenpackage;
+
+public class LoopDebugging {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		for(i=1;i<=5;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
